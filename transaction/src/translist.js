03783e2d989f647656d8d03b9007bbc79c23910b
leroy <leroy.kariuki@student.moringaschool.com>
2023-07-24 21:06:39 +0300

    donewith assignment

--- transaction/src/translist.js
+++ transaction/src/translist.js
@@ -4,4 +4,21 @@ import React, { useState, useEffect } from "react";
 
 const TransactionList = () => {
   const [transactions, setTransactions] = useState([]);
-}
+
+  useEffect(() => {
+    fetch("/db.json") // Assumes db.json is in the public folder
+      .then((response) => response.json())
+      .then((data) => setTransactions(data.transactions))
+      .catch((error) => console.error("Error fetching data: ", error));
+  }, []);
+
+  return (
+    <div>
+      {/* Render the component that will display the list of transactions */}
+      {/* Pass the transactions data as props */}
+      <DisplayTransactions transactions={transactions} />
+    </div>
+  );
+};
+
+export default TransactionList;
